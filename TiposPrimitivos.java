705a815bee39e595fdcf3fbd6b480389e48402d5
Igor-Brandalise <brandaliseigor123@gmail.com>
2025-07-25 23:00:41 -0300

    tipos primitivos

---
+++ TiposPrimitivos.java
@@ -0,0 +1,28 @@
+public class TiposPrimitivos {
+    public static void main(String[] args) {
+        // Tipos inteiros
+        byte idade = 25;
+        short populacaoPequenaCidade = 15000;
+        int populacaoBrasil = 203000000;
+        long populacaoMundial = 8000000000L;
+
+        // Tipos decimais
+        float temperatura = 36.6f;
+        double preco = 1999.99;
+
+        // Tipo caractere
+        char letra = 'A';
+
+        // Tipo booleano
+        boolean ligado = true;
+
+        System.out.println("byte: " + idade);
+        System.out.println("short: " + populacaoPequenaCidade);
+        System.out.println("int: " + populacaoBrasil);
+        System.out.println("long: " + populacaoMundial);
+        System.out.println("float: " + temperatura);
+        System.out.println("double: " + preco);
+        System.out.println("char: " + letra);
+        System.out.println("boolean: " + ligado);
+    }
+}
