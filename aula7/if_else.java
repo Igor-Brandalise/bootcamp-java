c0508c944a0319dd4d0f948ea6a89622d556d689
Igor-Brandalise <brandaliseigor123@gmail.com>
2025-08-05 15:09:51 -0300

    if / else

---
+++ aula7/if_else.java
@@ -0,0 +1,15 @@
+
+import java.util.Scanner;
+
+public class if_else {
+    public static void main(String[] args) {
+        var scanner = new Scanner(System.in);
+        System.out.println("Qual sua idade");
+        var age = scanner.nextInt();
+
+        if (age >= 18) {
+            System.out.printf("Você tem %s anos \n", age);
+        }
+
+    }
+}
