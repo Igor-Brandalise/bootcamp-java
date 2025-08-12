00a316431353ce30d756ca3c69397275800bf01d
Igor Brandalise <brandaliseigor123@gmail.com>
2025-08-12 17:07:44 -0300

    classe

---
+++ aula9/Main.java
@@ -0,0 +1,16 @@
+public class Main {
+    public static void main(String[] args) {
+        // Criando objetos da classe Cachorro
+        Cachorro dog1 = new Cachorro("Rex", 3);
+        Cachorro dog2 = new Cachorro("Luna", 5);
+
+        // Usando métodos
+        dog1.mostrarInfo();
+        dog1.latir();
+
+        System.out.println();
+
+        dog2.mostrarInfo();
+        dog2.latir();
+    }
+}
